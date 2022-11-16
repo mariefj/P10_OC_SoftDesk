d461c6c8cbfdee4ae68faf97006988f93d8b6667
mariefj <marie.forja@gmail.com>
2022-11-16 18:45:28 +0100

    feat(): change duration token + refacto permission

--- tracking/permissions.py
+++ tracking/permissions.py
@@ -5,9 +5,7 @@ class IsAuthor(BasePermission):
     def has_object_permission(self, request, view, obj):
         if (request.method in SAFE_METHODS):
             return True
-        if obj.author_user:
-            return obj.author_user == request.user
-        return view.get_project().author_user == request.user
+        return obj.author_user == request.user
 
 class IsContributorAuthor(BasePermission):
 
